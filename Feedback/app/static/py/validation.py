4b349e389bf222a13a19db5c230b39e05546c3ba
Andrew Millar <andrewjcmillar@gmail.com>
2016-12-07 10:21:13 +0000

    adding alert boxes for validation

--- Feedback/app/static/py/validation.py
+++ Feedback/app/static/py/validation.py
@@ -1,11 +1,23 @@
+from tkinter import *
+from tkinter import messagebox
+import tkinter
+
 def feedbackValidation(feedback):
+    ##### confirms that comment is under 1200 characters #####
     if (len(feedback)>1200):
+        root = tkinter.Tk()
+        root.withdraw()
+        tkinter.messagebox.showwarning('Invalid Input', 'Please Limit your comment to 1200 characters')
         return True
     else:
         return False
 
 def satisfactionValidation(satisfaction):
+    ##### confirms that user has selected one of the radio button options #####
     if (satisfaction == "None"):
+        root = tkinter.Tk()
+        root.withdraw()
+        tkinter.messagebox.showwarning('Invalid Input', 'Please select a satisfaction option')
         return True
     else:
         return False
--- Feedback/app/views.py
+++ Feedback/app/views.py
@@ -24,10 +24,12 @@ def database_Send():
     conn_string = "host='172.28.78.195' port='5432' dbname='feedback2' user='andrew' password='password'"
     conn = dbConnect(conn_string)
 
+    ## retrieves data from form ##
     satisfaction = form.satisfaction.data
     feedback = form.feedback.data
     date = datetime.now()
 
+    ## validation ##
     if (feedbackValidation(feedback) or satisfactionValidation(satisfaction)):
         return redirect('/index')
 
